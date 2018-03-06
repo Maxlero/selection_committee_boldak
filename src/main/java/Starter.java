e3b36ce948660a740975a1b1a1076eeb8f1b5fd1
powerfuldeff <powerfuldeff@gmail.com>
2018-03-06 11:13:36 +0400

    Initial commit

---
+++ src/main/java/Starter.java
@@ -0,0 +1,9 @@
+import org.apache.log4j.Logger;
+
+public class Starter {
+	private static final Logger logger = Logger.getLogger(Starter.class);
+
+	public static void main(String args[]) {
+		logger.info("Hello this is test");
+	}
+}
